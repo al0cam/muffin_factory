623e86325688d70e671dfdc4ee1f5edbb85c1b53
Al0cam <benjaminsikac@gmail.com>
2022-07-22 22:12:29 +0200

    generate muffin scaffold

---
+++ app/views/muffins/_muffin.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! muffin, :id, :name, :price, :description, :created_at, :updated_at
+json.url muffin_url(muffin, format: :json)
---
+++ db/migrate/20220722201130_create_muffins.rb
@@ -0,0 +1,11 @@
+class CreateMuffins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :muffins do |t|
+      t.string :name
+      t.float :price
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
